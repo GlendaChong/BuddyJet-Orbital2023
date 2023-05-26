4c369a3de01bbf8a91429bb566cbcd830b8f29b8
GlendaChong <glendachong04@gmail.com>
2023-05-27 02:33:59 +0800

    Add authentication

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import "expo-router/entry";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingScreen from "./app";
+import Login from "./app/(authentication)/Login";
+import CreateAccount from "./app/(authentication)/CreateAccount";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Onboarding Screen" component={OnboardingScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Create Account" component={CreateAccount} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); 
+}
