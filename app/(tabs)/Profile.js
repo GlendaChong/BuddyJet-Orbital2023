4f7f0dd643607d0899d4601a0e26e3bd0a747be9
GlendaChong <glendachong04@gmail.com>
2023-05-28 20:54:05 +0800

    Add home page tabs bar and logout feature

---
+++ app/(tabs)/Profile.js
@@ -0,0 +1,17 @@
+import { Text} from "react-native";
+import { Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { supabase } from "../../lib/supabase";
+
+export default function Profile() {
+    const handleLogOut = async () => {
+        await supabase.auth.signOut(); 
+    }
+    
+    return (
+        <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>This is the profile page. Work in progress!</Text>
+            <Button onPress={handleLogOut}>Logout</Button>
+        </SafeAreaView>
+    ); 
+}
