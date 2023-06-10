17ef4fb5e6cb4b07cc88a337722bf6be1734ce71
tayruxin <tayruxin00@gmail.com>
2023-06-11 00:06:33 +0800

    Edits to layout files

--- app/(tabs)/(budgetTabs)/CreateBudget.jsx
+++ app/(tabs)/(budgetTabs)/CreateBudget.jsx
@@ -2,8 +2,7 @@ import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from 'expo-router';
 
-export default function Dashboard() {
-
+const createBudget = ({route}) => {
     return (
         <Stack>
             <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -12,3 +11,5 @@ export default function Dashboard() {
         </Stack>
     ); 
 }
+
+export default createBudget 
--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,41 +1,34 @@
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { useNavigation } from '@react-navigation/native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./Home";
 import Budget from "./(budgetTabs)/Budget";
 import Dashboard from "./Dashboard";
 import Profile from "./Profile";
+import {BudgetScreenNavigator} from './CustomNavigation'
 import CreateBudget from "./(budgetTabs)/CreateBudget";
+import { NavigationContainer } from "@react-navigation/native";
 
 
 export const unstable_settings = {
     initialRouteName: "Home",
 };
 
-export default function HomeLayout() {
+const HomeLayout = () => {
     const Tab = createBottomTabNavigator(); 
 
     return (
+        <>
+        <NavigationContainer>
         <Tab.Navigator>
             <Tab.Screen name="Home" component={Home}/>
             <Tab.Screen name="Budget" component={Budget}/>
             <Tab.Screen name="Dashboard" component={Dashboard} />
             <Tab.Screen name="Profile" component={Profile}/>
-            {/* <Tab.Screen name ="CreateBudget" component={CreateBudget}/> */}
         </Tab.Navigator>
+        </NavigationContainer>
+        </>
 
     );
 }
 
-const BudgetStackNavigator = createStackNavigator();
-
-function BudgetStack() {
-  return (
-    <BudgetStackNavigator.Navigator>
-      <BudgetStackNavigator.Screen name="CreateBudget" component={CreateBudget} />
-    </BudgetStackNavigator.Navigator>
-  );
-}
-
+export default HomeLayout;
