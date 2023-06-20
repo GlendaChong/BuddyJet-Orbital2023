44c544de062772ba3c005617296f8d8b3d4f49b5
tayruxin <tayruxin00@gmail.com>
2023-06-20 12:05:47 +0800

    Budget tab front end

--- app/(budgetTabs)/CreateBudget.jsx
+++ app/(budgetTabs)/CreateBudget.jsx
@@ -1,59 +1,178 @@
-import { Text, StyleSheet} from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { Button } from 'react-native-paper';
 import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from 'expo-router';
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 // import TextFieldInput from "../(authentication)/TextFieldInput";
 import TextFieldInput from "./TextFieldInput";
+import BackButton from "../components/BackButton";
 import { ScrollView } from "react-native-gesture-handler";
-
+import { supabase } from "../../lib/supabase";
 
 
 function CreateBudget() {
     const router = useRouter();
+    const [loading, setLoading] = useState(true)
     const [income, setIncome] = useState(''); 
+    const [errMsg, setErrMsg] = useState('');
 
-    const handleSubmit = async () => {
+    //handle the submission for budget 1
+    const handleSubmit1 = async () => {
+
+      //obtain the user_id from the profile database 
+      let { data: profiles } = await supabase
+      .from('profiles')
+      .select('id')
+
+      const selectedID = profiles[0]?.id;
+
         if (income == '') {
             setErrMsg("Income cannot be empty")
             return;
         }
+
+        setLoading(true)
+
+        const updates = {
+          income,
+          user_id: selectedID,
+          save: 0.2,
+          spending: 0.8, 
+        }
+
+        // insert the data into the budget table 
+          await supabase.from('budget').insert([updates]);
+
+        //obtain the budget_id that is currently in use 
+        let { data: budget } = await supabase
+        .from('budget')
+        .select('budget_id')
+        .eq('in_use', true)
+
+        const budgetID = budget[0]?.budget_id
+        console.log(budget)
+
+          // insert data into category table
+          const {data, error} = 
+          await supabase.from('categories').insert([
+            {user_id: selectedID, budget_id: budgetID, category:'Food', spending:0.25, color:'#BF5AF2'}
+            , {user_id: selectedID, budget_id: budgetID, category:'Transport', spending:0.15, color:'#0A84FF'}
+            , {user_id: selectedID, budget_id: budgetID, category:'Recreation', spending:0.3,color:'#F46040' }
+            , {user_id: selectedID, budget_id: budgetID, category:'Bills', spending:0.10, color:'#32D74B'}
+          ]);
+        
+        if (error) {
+          alert(error.message)
+
+        }
+        setLoading(false)
+
     }
+
 
     const MakeBudgetButton = () => {
         return (
+        
+          <View>
+            <Text style={{color: '#fff', opacity:0.1, marginTop: -75}}> anchor </Text>
+            <View style={{bottom:140, paddingHorizontal: 28, paddingVertical: 18}}>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <View style={{flex: 1, height: 1, backgroundColor: '#C2C8D0'}} />
+              <View>
+              <Text style={{width: 50, textAlign: 'center', color:'#2D333A'}}>OR</Text>
+              </View>
+              <View style={{flex: 1, height: 1, backgroundColor: '#C2C8D0'}} />
+              </View>
+            </View>
           <Button
           mode="contained" 
           style={styles.MakeBudgetButton}
           labelStyle={styles.MakeBudgetText}
-          onPress={() => router.push('./MakeBudget')}
+          onPress={() => {
+            router.push('./MakeBudget')
+          }
+          }
           >
             Make your own budget
           </Button>
+          </View>
+      
+        ); 
+      };
+
+      const Budget1Desc = () => {
+        return (
+          <View>
+            <Text style={{color: '#fff', opacity:0.1, marginTop: -60}}> anchor </Text>
+            <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 15, bottom: 260, left: 20}}>Save: </Text>
+            <Text style={{fontFamily:'Poppins-Medium', color:'#2C2646', fontSize: 15, bottom: 280, left: 92}}>20% </Text>
+            <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 15, bottom: 270, left: 20}}>Spend: </Text>
+            <Text style={{fontFamily:'Poppins-Medium', color:'#2C2646', fontSize: 15, bottom: 290, left: 92}}>80% </Text>
+            <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 15, bottom: 270, left: 20}}>Spending: </Text>
+            <FontAwesomeIcon
+              icon={faChevronRight}
+                style={{bottom: 355, left:250}}
+                size={25}
+            />
+            <Text onPress={()=> {router.push('../(home)/Budget'), handleSubmit1()}} style={{bottom: 375, left:250, opacity:0.1, color:'#F3F6FA'}} >
+                back
+            </Text>
+            <FontAwesomeIcon
+              icon={faCircle}
+                style={{color:'#BF5AF2',bottom: 280, left: 28}}
+                size={15}
+            />
+            <FontAwesomeIcon
+              icon={faCircle}
+                style={{color:'#0A84FF',bottom: 250, left: 29}}
+                size={15}
+            />
+             <FontAwesomeIcon
+              icon={faCircle}
+                style={{color:'#F46040',bottom: 220, left: 29}}
+                size={15}
+            />
+             <FontAwesomeIcon
+              icon={faCircle}
+                style={{color:'#32D74B',bottom: 188, left: 29}}
+                size={15}
+            />
+             <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 15, bottom: 344, left: 70}}>Food: 25% </Text>
+             <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 15, bottom: 320, left: 70}}>Transport: 15% </Text>
+             <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 15, bottom: 298, left: 70}}>Recreation: 30% </Text>
+             <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 15, bottom: 268, left: 70}}>Bills: 10% </Text>
+          </View>
+        ); 
+      };
+
+      const SampleBudget1 = () => {
+        
+        return (
+          <View style={{padding: 35}}>
+            <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize: 18}}>Sample Budget 1</Text>
+            <Text style={{fontFamily:'Poppins-Regular', color:'#2C2646', fontSize: 13, top: 10}}>Basic need budget</Text>
+            <View style={styles.roundedRect} />
+            <Budget1Desc />
+            {/* {errMsg !== "" && <Text>{errMsg}</Text>} */}
+      
+          </View>
         ); 
       };
     
   return (
     <SafeAreaView style={{ flex:1, backgroundColor: '#fff' }}>
       <ScrollView>
-      <FontAwesomeIcon
-        icon={faChevronLeft}
-        
-        style={styles.BackButton}
-        size={20}
-      />
-      <Text
-        onPress={() => {router.back()}}
-        style={styles.BudgetText}
-      >
-        Budget
-      </Text>
-      <Text style={styles.CreateText}>Create</Text>
-      <Text style={styles.DescriptionText}>A budget</Text>
-      <TextFieldInput label='Income' value={income} onChangeText={setIncome} />
-      <MakeBudgetButton />
+        <BackButton />
+        <Text style={styles.CreateText}>Create</Text>
+        <Text style={styles.DescriptionText}>A budget</Text>
+        <TextFieldInput label='Income' value={income} onChangeText={setIncome} />
+        <SampleBudget1 />
+        <View style ={{marginTop:-45, marginBottom: -90}}>
+        <MakeBudgetButton />
+        </View>
       </ScrollView>
     </SafeAreaView>
 
@@ -66,11 +185,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
       },
 
-      BackButton: {
-        position: 'absolute', 
-        marginTop: 7,
-        left: 30
-      }, 
 
       CreateText: {
         position: 'absolute', 
@@ -84,18 +198,6 @@ const styles = StyleSheet.create({
         alignContent: 'center',
         color: '#100D40', 
     }, 
-
-      BudgetText: {
-        fontFamily: 'Poppins-Regular', 
-        fontWeight: 600, 
-        fontSize: 15, 
-        position: 'absolute', 
-        marginTop: 7, 
-        // left: 55,
-        left: 20,
-        color: '#fff', 
-        opacity: 0.1, 
-      }, 
 
       DescriptionText: {
         position: 'absolute', 
@@ -111,13 +213,23 @@ const styles = StyleSheet.create({
         
     }, 
 
+    roundedRect: {
+      width: 311,
+      height: 350,
+      backgroundColor: '#F3F6FA',
+      borderRadius: 18,
+      top: 20,
+      alignContent:'center',
+    },
+
       MakeBudgetButton: {
         backgroundColor: '#3D70FF',
         borderRadius: 40, 
         width: 327, 
         height: 56, 
         left: 30,
-        marginTop: 800, 
+        bottom: 130
+    
     },
       MakeBudgetText: {
         color: 'white', 
--- app/(home)/Budget.jsx
+++ app/(home)/Budget.jsx
@@ -3,10 +3,17 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet, View, Image } from 'react-native';
 import { Button} from 'react-native-paper';
 import { useRouter } from 'expo-router';
-
+import { useState, useEffect } from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import { supabase } from "../../lib/supabase";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 
 export default function Budget() {
     const router = useRouter();
+    let [boolean, setBoolean] = useState(true);
+
+
+    //component for no budget
 
     const CreateBudgetButton = () => {
         return (
@@ -14,7 +21,10 @@ export default function Budget() {
           mode="contained" 
           style={styles.createBudgetButton}
           labelStyle={styles.createBudgetText}
-          onPress={() => router.push('../(budgetTabs)/CreateBudget')}
+          onPress={() => {
+            router.push('../(budgetTabs)/CreateBudget')
+            // setBoolean(true)
+          }}
           >
             Create a budget
           </Button>
@@ -23,7 +33,7 @@ export default function Budget() {
 
       const Design = () => {
         return (
-          <View>
+          <View style={{marginTop: 80}}>
             <Image style={styles.image} source={require('../../assets/budget.jpeg')} />
             <Text style={styles.mainText}>No Budget</Text>
             <Text style={styles.descriptionText}>You have not created a budget</Text>
@@ -31,13 +41,117 @@ export default function Budget() {
         ); 
       }
 
+      //component for when there is budget 
+      const [category, setCategory] = useState([]);
+      const [monthlyBudget, setMonthlyBudget] = useState(0);
+      const [budgetId, setbudgetId] = useState(0)
+
+      //obtain the budget_id that is currently in use 
+      const checkBudget = async () => {
+
+        let { data: budget } = await supabase
+            .from('budget')
+            .select('budget_id')
+            .eq('in_use', true);
+
+        const budget_id = budget[0]?.budget_id
+        // console.log(budget_id)
+        setbudgetId(budget_id);
+
+          if (budget_id == undefined) {
+            setBoolean(false);
+          }
+          console.log(boolean)
+      }
+
+      useEffect(() => {
+        checkBudget();
+      }, [])
+
+    
+
+    const fetchBudgetDetail = async () => {
+
+      let {data: budget} = await supabase.from('budget').select('income, spending').eq('in_use', true).eq('budget_id', budgetId)
+
+      const income = parseInt(budget[0]?.income);
+      const spend = budget[0]?.spending;
+      const budgetAmount = income * spend;
+
+      console.log(income)
+      console.log(spend)
+      console.log(budgetAmount)
+
+      setMonthlyBudget(budgetAmount);
+      
+    }
+
+     useEffect(() => {
+        fetchBudgetDetail();
+      }, []);
+    
+    const fetchCategoryDetail = async () => {
+
+      let {data: categoryData} = await supabase.from('categories').select('category, spending, color').eq('in_use', true)
+      console.log(category)
+      setCategory(categoryData);
+
+    }
+
+    useEffect(() => {
+      fetchCategoryDetail();
+    }, []);
+
+
+    const BudgetBox = () => {
+      return (
+        <View style={{ marginTop: -15, flex: 1}}>
+          <Text style={{fontFamily: 'Poppins-Regular' , left: 280, marginBottom:10}}> Edit</Text> 
+      <View  style={{backgroundColor: '#000E90', borderRadius: 18, paddingHorizontal: 30, paddingTop: 15, paddingBottom:8}}>
+        {category.map((item, index) => (
+          <View key={index} style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 15 }} >
+              <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: item.color, marginRight: 20 }} />
+              <Text style={{ fontFamily: 'Poppins-Medium', width: 120, color:'#fff'}}>{item.category}</Text>
+              <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize:18, width: 85, color:'#fff'}}>${(monthlyBudget * item.spending).toFixed(0)}</Text>
+              <Text style={{ fontFamily: 'Poppins-Medium', width: 30, color:'#fff'}}>{`${(item.spending * 100).toFixed(0)}%`}</Text>
+          </View>
+        ))}
+   
+        {/* <Text onPress={()=> {router.push("../(budgetTabs)/BudgetBoard")}} >
+                test
+            </Text> */}
+     </View>
+     </View>
+      );
+    }
+
+    //checking if there is budget
+   if (boolean) {
+    //The page for when there is a budget 
+    return (
+      <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" , backgroundColor: "#fff"}}>
+        <ScrollView>
+          <View style={{ alignItems: 'center' }}>
+        <Text style={{fontFamily:'Poppins-Regular', color:'#2C2646', marginTop: 20}}>Monthly Budget</Text>
+        <Text style={{fontFamily:'Poppins-SemiBold', color:'#2C2646', fontSize:48, marginTop: 0}}>${monthlyBudget}</Text>
+        <BudgetBox />
+        </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+    } else {
+    //The page for when there is  no budget
     return (
         <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" , backgroundColor: "#fff"}}>
             <Design />
             <CreateBudgetButton /> 
+
         </SafeAreaView>
     ); 
+      }
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
