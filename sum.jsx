75b78bd939c7059308c86fe37c8abb4f94fbab35
tayruxin <tayruxin00@gmail.com>
2023-07-02 23:01:53 +0800

    Started unit and widget testing Unit testing was performed on Onboarding screen and authentication

--- sum.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
-import { Text, Button, ActivityIndicator } from "react-native-paper";
-import { useRouter } from "expo-router"
-
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
