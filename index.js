7672827ca6a1aad04a2cc3993204e8f02a1186c4
GlendaChong <glendachong04@gmail.com>
2023-07-04 21:35:17 +0800

    Removed queryClient file

--- index.js
+++ index.js
@@ -5,22 +5,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeLayout from "./app/(home)/_layout";
 import AuthLayout from "./app/(authentication)/_layout";
-import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "./queryClient";
-
 
 function App() {
   const Stack = createNativeStackNavigator();
-  
+
   return (
-    <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen name="Onboarding Screen" component={AuthLayout} />
-              <Stack.Screen name="Home" component={HomeLayout} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    </QueryClientProvider>
+      <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Onboarding Screen" component={AuthLayout} />
+            <Stack.Screen name="Home" component={HomeLayout} />
+          </Stack.Navigator>
+      </NavigationContainer>
    
   ); 
 }
