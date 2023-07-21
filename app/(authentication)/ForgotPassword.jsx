45f868996791a6565824bc045524e7c72117968c
GlendaChong <glendachong04@gmail.com>
2023-07-22 02:07:12 +0800

    Updated forget password feature

--- app/(authentication)/ForgotPassword.jsx
+++ app/(authentication)/ForgotPassword.jsx
@@ -1,101 +1,130 @@
-import { useState, useEffect } from "react";
-import React from 'react';
-import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Image, TouchableOpacity, Alert } from "react-native";
 import { Text, Button, ActivityIndicator } from "react-native-paper";
 import { supabase } from "../../lib/supabase";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TextFieldInput from "../components/TextFieldInput"
 import { ScrollView } from "react-native-gesture-handler";
-import BackButton from '../components/BackButton'
-import { useRouter } from "expo-router";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import * as Linking from 'expo-linking'
 
 function ForgotPassword() {
-    const router = useRouter();
-    const [email, setEmail] = useState('');;
+    const [email, setEmail] = useState('');
+    const [otp, setOTP] = useState(''); 
     const [loading, setLoading] = useState(false);
     const [errMsg, setErrMsg] = useState('');
+    const [isEmailSent, setIsEmailSent] = useState(false);
 
-    let redirectURL = Linking.createURL('ResetPassword')
-    console.log(redirectURL)
-
-    const handleSubmit = async () => {
-        router.push('./ResetPassword')
-
+    // Supabase to send an email with OTP
+    const handleSendOTP = async () => {
         setErrMsg('');
-        if (email === '') {
-            setErrMsg("Email cannot be empty");
-            return;
+        try {
+            if (email === '' ) {
+                  setErrMsg("Email cannot be empty");
+                  return;
+            } 
+    
+            setLoading(true);
+            const { error } = await supabase.auth.signInWithOtp({ email: email });
+            setLoading(false);
+            setIsEmailSent(true);
+
+        } catch (error) {
+            setErrMsg("Error sending email: " + error);
         }
-        setLoading(true);
-        // const { error } = await supabase.auth.resetPasswordForEmail(email)
-
-        /**
-         * Step 1: Send the user an email to get a password reset token.
-         * This email contains a link which sends the user back to your application.
-         */
-        const { data, error } = await supabase.auth
-            .resetPasswordForEmail(email)
-
-
-        setLoading(false);
-
-        if (error) {
-            setErrMsg(error.message);
-            return;
-        }
-
     };
 
-    /**
-      * Step 2: Once the user is redirected back to your application,
-      * ask the user to reset their password.
-      */
-    useEffect(() => {
-        supabase.auth.onAuthStateChange(async (event, session) => {
-            if (event == "PASSWORD_RECOVERY") {
-                const newPassword = prompt("What would you like your new password to be?");
-                const { data, error } = await supabase.auth
-                    .updateUser({ password: newPassword })
+    // Verify OTP to confirm user account
+    const handleVerifyOTP = async () => {
+        setErrMsg('');
 
-                if (data) alert("Password updated successfully!")
-                if (error) alert("There was an error updating your password.")
+        try {
+            setLoading(true);
+            // Use the token as the OTP for password reset
+            const { error } = await supabase.auth.verifyOtp({ email: email, token: otp, type: 'email' });
+            setLoading(false);
+
+            if (!error) {
+                // OTP verification successful
+                // Show the alert message for password reset
+                Alert.alert(
+                    "OTP Verification Successful",
+                    "Please proceed to change your password at the Profile Tab.",
+                    [
+                        { text: "OK", onPress: () => console.log("OK Pressed") }
+                    ]
+                );
             }
-        })
-    }, [])
+    
+        } catch (error) {
+            setErrMsg("Error logging in with OTP: " + error);
+        }
+    };
 
+        
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-            <ScrollView>
-                <TouchableOpacity style={{ marginLeft: 30, marginTop: 20 }} onPress={() => { router.back() }}><FontAwesomeIcon
-                    icon={faChevronLeft}
-                    size={20}
-                /></TouchableOpacity>
-                <View style={{ alignItems: 'center' }}>
-                    <Text style={styles.welcomeText}>Forgot Password</Text>
-                    <Image style={{ marginTop: 60 }} source={require('../../assets/changePassword.jpg')} />
-                    <Text style={styles.descriptionText}>Enter your registered email below to receive password reset instructions</Text>
-                </View>
-                <TextFieldInput label='Email' value={email} onChangeText={setEmail} />
-                <Button
-                    style={styles.loginButton}
-                    labelStyle={styles.loginText}
-                    onPress={handleSubmit}
-                >
-                    Reset Password
-                </Button>
-                <View style={{ alignItems: 'center', marginTop: 15, marginHorizontal: 30 }}>
-                    {errMsg !== " " && <Text>{errMsg}</Text>}
-                    {loading && <ActivityIndicator />}
-                </View>
-            </ScrollView>
+            {!isEmailSent ? (
+                <ScrollView>
+                    <TouchableOpacity style={{ marginLeft: 30, marginTop: 20 }} onPress={() => { router.back() }}>
+                        <FontAwesomeIcon
+                        icon={faChevronLeft}
+                        size={20}
+                        />
+                    </TouchableOpacity>
+                    <View style={{ alignItems: 'center' }}>
+                        <Text style={styles.welcomeText}>Forgot Password</Text>
+                        <Image style={styles.image} source={require('../../assets/changePassword.jpg')} />
+                        <Text style={styles.descriptionText}>Enter your registered email below to receive password reset instructions</Text>
+                    </View>
+                    <TextFieldInput label='Email' value={email} onChangeText={setEmail} />
+                    <Button
+                        style={styles.resetButton}
+                        labelStyle={styles.resetText}
+                        onPress={handleSendOTP}
+                    >
+                        Reset Password
+                    </Button>
+                    <View style={{ alignItems: 'center', marginTop: 15, marginHorizontal: 30 }}>
+                        {errMsg !== " " && <Text>{errMsg}</Text>}
+                        {loading && <ActivityIndicator />}
+                    </View>
+                </ScrollView>
+            )  : (
+                <ScrollView>
+                    <TouchableOpacity style={{ marginLeft: 30, marginTop: 20 }} onPress={() => { router.back() }}>
+                        <FontAwesomeIcon
+                        icon={faChevronLeft}
+                        size={20}
+                        />
+                    </TouchableOpacity>
+                    <View style={{ alignItems: 'center' }}>
+                        <Text style={styles.welcomeText}>Verify Your OTP</Text>
+                        <Image style={styles.image} source={require('../../assets/changePassword.jpg')} />
+                        <Text style={styles.descriptionText}>
+                            A 6-digit code has been set to your email: {email}. 
+                            Once signed in, please proceed to change your password. 
+                        </Text>
+                    </View>
+                    <TextFieldInput label='OTP' value={otp} onChangeText={setOTP} />
+                    <Button
+                        style={styles.resetButton}
+                        labelStyle={styles.resetText}
+                        onPress={handleVerifyOTP}
+                    >
+                        Reset
+                    </Button>
+                    <View style={{ alignItems: 'center', marginTop: 10 }}>
+                        {errMsg !== "" && <Text>{errMsg}</Text>}
+                        {loading && <ActivityIndicator />}
+                    </View>
+                </ScrollView>      
+            )}
         </SafeAreaView>
     );
 }
 
-const styles = {
+const styles = StyleSheet.create({
     welcomeText: {
         fontFamily: 'Poppins-SemiBold',
         fontWeight: 600,
@@ -149,8 +178,14 @@ const styles = {
         fontFamily: 'Poppins-SemiBold',
         fontWeight: 600,
         fontSize: 18,
-        lineHeight: 35,
+        lineHeight: 40,
     },
-}
+    image: {
+        width: 300,
+        height: 230,
+        alignSelf: "center",
+    },
+});
+
 
 export default ForgotPassword; 
