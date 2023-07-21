d322310c3617af5adc0827cfb4383cc86519eeb4
tayruxin <tayruxin00@gmail.com>
2023-07-21 17:56:12 +0800

    Income check for input from CreateBudget Checks if the input is indeed integers else alert user

--- __tests__/CreateBudget.test.js
+++ __tests__/CreateBudget.test.js
@@ -3,6 +3,12 @@ import CreateBudget from "../app/(home)/Budget/CreateBudget";
 import { supabase } from "../lib/supabase";
 import SampleBudget from "../app/components/SampleBudget";
 import { TouchableOpacity } from "react-native";
+import { useRouter } from "../__mocks__/expo-router";
+import { Alert } from "react-native";
+
+jest.mock("../__mocks__/expo-router", () => ({
+  useRouter: jest.fn(),
+}));
 
 // Mock the supabase dependency
 jest.mock("../lib/supabase", () => ({
@@ -30,18 +36,24 @@ jest.mock(
   () => {
     const { View } = require("react-native");
     const TouchableOpacityMock = (props) => {
-      return <View {...props} />;
+      return <View {...props} testID="submitButton" />;
     };
     TouchableOpacityMock.displayName = "TouchableOpacity";
+    // TouchableOpacityMock.testID = "submit-button";
 
     return TouchableOpacityMock;
   }
 );
 
 describe("CreateBudget", () => {
+  useRouter.mockReturnValue({
+    push: jest.fn(), // Mock the push method
+  });
+
+  const handleSubmit = jest.fn();
+
   it("should call handleSubmit and navigate to another page on button press", async () => {
-    const handleSubmit = jest.fn();
-    const { getByTestId } = render(
+    const { getByTestId, debug } = render(
       <SampleBudget
         index={1}
         budgetType="Sample Budget"
@@ -53,8 +65,12 @@ describe("CreateBudget", () => {
         handleSubmit={handleSubmit}
       />
     );
+
+    // Print the rendered output to the console for debugging
+    debug();
+
     await waitFor(() => {
-      const button = getByTestId("submit-button");
+      const button = getByTestId("submitButton");
       fireEvent.press(button);
 
       expect(handleSubmit).toHaveBeenCalled();
@@ -62,39 +78,84 @@ describe("CreateBudget", () => {
     });
   });
 
-  //   it("should display an error message when income is empty", async () => {
-  //     const { getByLabelText, getByText, queryByText, getByTestId } = render(
-  //       <CreateBudget />
-  //     );
+  it("should display an error message when income is empty", async () => {
+    const { queryAllByTestId, debug, getByTestId } = render(<CreateBudget />);
 
-  //     // Set up the input fields
-  //     const incomeInput = getByTestId("text-input-flat");
+    // Spy on the Alert component to capture if it was called
+    jest.spyOn(Alert, "alert");
 
-  //     // Enter the income value
-  //     fireEvent.changeText(incomeInput, "");
+    // Set up the input fields
+    const incomeInput = getByTestId("text-input-flat");
 
-  //     // Trigger the form submission
+    // Enter the income value
+    fireEvent.changeText(incomeInput, "");
+
+    await waitFor(() => {
+      // Get all elements with testID "submitButton"
+      const buttons = queryAllByTestId("submitButton");
+
+      // Check if there's at least one element with the testID "submitButton"
+      expect(buttons.length).toBeGreaterThan(0);
+
+      // Trigger the form submission using the first element with testID "submitButton"
+      fireEvent.press(buttons[0]);
+    });
+
+    await waitFor(() => expect(Alert.alert).toHaveBeenCalled());
+
+    // Check if the alert was called with the correct message
+    expect(Alert.alert).toHaveBeenCalledWith(
+      "Missing Information",
+      "Please remember to key in your fixed income",
+      [{ style: "okay", text: "Okay" }]
+    );
+  });
+
+  it("should display an error message when income format is wrong", async () => {
+    const { queryAllByTestId, debug, getByTestId } = render(<CreateBudget />);
+
+    // Spy on the Alert component to capture if it was called
+    jest.spyOn(Alert, "alert");
+
+    // Set up the input fields
+    const incomeInput = getByTestId("text-input-flat");
+
+    // Enter the income value
+    fireEvent.changeText(incomeInput, "ddd");
+
+    await waitFor(() => {
+      // Get all elements with testID "submitButton"
+      const buttons = queryAllByTestId("submitButton");
+
+      // Check if there's at least one element with the testID "submitButton"
+      expect(buttons.length).toBeGreaterThan(0);
+
+      // Trigger the form submission using the first element with testID "submitButton"
+      fireEvent.press(buttons[0]);
+    });
+
+    await waitFor(() => expect(Alert.alert).toHaveBeenCalled());
+
+    // Check if the alert was called with the correct message
+    expect(Alert.alert).toHaveBeenCalledWith(
+      "Invalid Information",
+      "Please key in a valid fixed income",
+      [{ style: "okay", text: "Okay" }]
+    );
+  });
+
+  // it("should call the updateData function with correct parameters", async () => {
+  //   const { getByText } = render(<CreateBudget />);
+
+  //   // Mock the functions and inputs
+  //   const handleSubmit = jest.fn();
+
+  //   // Trigger the form submission
+  //   act(() => {
   //     fireEvent.press(getByText("Make Budget"));
-
-  //     // Assert the error message is displayed
-  //     const errorMessage = await waitFor(() =>
-  //       queryByText("Please remember to key in your fixed income")
-  //     );
-  //     expect(errorMessage).toBeTruthy();
   //   });
 
-  //   it("should call the updateData function with correct parameters", async () => {
-  //     const { getByText } = render(<CreateBudget />);
-
-  //     // Mock the functions and inputs
-  //     const handleSubmit = jest.fn();
-
-  //     // Trigger the form submission
-  //     act(() => {
-  //       fireEvent.press(getByText("Make Budget"));
-  //     });
-
-  //     // Assert the updateData function is called with correct parameters
-  //     expect(handleSubmit).toHaveBeenCalled();
-  //   });
+  //   // Assert the updateData function is called with correct parameters
+  //   expect(handleSubmit).toHaveBeenCalled();
+  // });
 });
