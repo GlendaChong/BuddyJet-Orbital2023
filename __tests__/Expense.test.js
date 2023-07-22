6e5577856f041a8acedb41868efc40121bbda1b0
tayruxin <tayruxin00@gmail.com>
2023-07-22 17:03:03 +0800

    Syntax edit to Expense test

--- __tests__/Expense.test.js
+++ __tests__/Expense.test.js
@@ -21,38 +21,12 @@ jest.mock("../lib/supabase", () => ({
   },
 }));
 
-// jest.mock('../lib/supabase', () => ({
-//   supabase: {
-//     from: jest.fn(() => ({
-//       select: jest.fn().mockReturnThis(),
-//       order: jest.fn().mockReturnThis(),
-//       gte: jest.fn().mockReturnThis(),
-//       lt: jest.fn().mockReturnThis(),
-//       then: jest.fn().mockResolvedValue({ data: [], status: 200 })
-//     })),
-//   },
-// }));
-
 // Mock the API functions
 jest.mock("../app/components/GetBackendData", () => ({
   GetMonthlyExpensesSortedByCat: jest.fn(),
   GetMonthlyExpensesSortedByDate: jest.fn(),
   GetMoneyIn: jest.fn(),
 }));
-
-// // Set up the mocked API responses
-// const mockedExpensesByCat = [
-//   { id: 1, category: 'Food', amount: 10 },
-//   { id: 2, category: 'Transport', amount: 20 },
-// ];
-
-// const mockedExpensesByDate = [
-//   { id: 1, date: '2023-07-01', amount: 10 },
-// ];
-
-// // Mock the API functions' implementation
-// GetMonthlyExpensesSortedByCat.mockResolvedValue(mockedExpensesByCat);
-// GetMonthlyExpensesSortedByDate.mockResolvedValue(mockedExpensesByDate);
 
 jest.mock(
   "react-native/Libraries/Components/Touchable/TouchableOpacity",
@@ -64,23 +38,6 @@ jest.mock(
     return TouchableOpacityMock;
   }
 );
-
-// describe('Expenses', () => {
-//   it('initializes the API client and makes a valid API call', async () => {
-
-//     const { queryByText } = render(<Expenses />);
-
-//     // Wait for the API calls to resolve
-//     await waitFor(() => {
-//       expect(GetMonthlyExpensesSortedByCat).toHaveBeenCalled();
-//       expect(GetMonthlyExpensesSortedByDate).toHaveBeenCalled();
-//     });
-
-//     // // Assert the response status code
-//     // expect(GetMonthlyExpensesSortedByCat.mock.results[0].value.status).toBe(200);
-//     // expect(GetMonthlyExpensesSortedByDate.mock.results[0].value.status).toBe(200);
-//   });
-// });
 
 describe("Test if user can make successful API calls", () => {
   test("GetMonthlyExpensesSortedByDate should return expenses sorted by date", async () => {
@@ -108,12 +65,6 @@ describe("Test if user can make successful API calls", () => {
 
     // Check the result and status code
     expect(result).toEqual(expenses);
-    // expect(supabase.from).toHaveBeenCalledWith("expenses");
-    // expect(supabase.from().select).toHaveBeenCalledWith("*");
-    // expect(supabase.from().order).toHaveBeenCalledWith("date", { ascending: false });
-    // expect(supabase.from().gte).toHaveBeenCalledWith(`${selectedYear}-${selectedMonth.padStart(2, "0")}-01`);
-    // expect(supabase.from().lt).toHaveBeenCalledWith(`${selectedYear}-${selectedMonth.padStart(2, "0")}-02`);
-    expect(response.status).toBe(200);
   });
   test("GetMonthlyExpensesSortedByDate should return expenses sorted by category", async () => {
     const selectedMonth = "July";
@@ -123,8 +74,6 @@ describe("Test if user can make successful API calls", () => {
     ];
     const response = { data: expenses, status: 200 };
 
-    // supabase.from().select().order().gte().lt().then.mockResolvedValue(response);
-
     GetMonthlyExpensesSortedByCat.mockResolvedValue(expenses);
 
     // Call the API function
@@ -133,7 +82,6 @@ describe("Test if user can make successful API calls", () => {
       selectedYear
     );
     expect(result).toEqual(expenses);
-    expect(response.status).toBe(200);
   });
 
   // Similar test cases for GetMonthlyExpensesSortedByCat, GetCurrentFixedIncome, and GetMoneyIn
